f63097e2a58367bd67587cd7a0323076a235afad
Michael D. Misamore <m.misamore@gmail.com>
2023-12-02 16:35:48 -0500

    Add code

---
+++ lfp_parsers/Maybe.fs
@@ -0,0 +1,32 @@
+﻿module Maybe
+
+/// The Maybe type constructor
+type Maybe<'a> = Nothing | Just of 'a
+
+/// Mapping over Maybe
+let map: ('a -> 'b) -> Maybe<'a> -> Maybe<'b> =
+    fun f ma -> match ma with
+                | Nothing -> Nothing
+                | Just a  -> Just (f a)
+
+/// Infix shorthand for map
+let (<!>) = map
+
+/// Bind for Maybe
+let bind: Maybe<'a> -> ('a -> Maybe<'b>) -> Maybe<'b> =
+    fun ma f -> match ma with
+                | Nothing -> Nothing
+                | Just a  -> f a
+
+/// Infix shorthand for bind
+let (>>=) = bind
+
+/// Return for Maybe
+let retMaybe: 'a -> Maybe<'a> = Just
+
+/// Apply for Maybe
+let ap: Maybe<'a -> 'b> -> Maybe<'a> -> Maybe<'b> =
+    fun mf ma -> mf >>= fun f -> ma >>= fun a -> retMaybe (f a)
+
+/// Infix shorthand for ap
+let (<*>) = ap
---
+++ lfp_parsers/Parser.fs
@@ -0,0 +1,30 @@
+﻿module Parser
+
+open Maybe
+
+/// Traditional string parser
+type Parser<'a> = string -> ('a * string) list
+
+/// Binding parsers together
+let bind: Parser<'a> -> ('a -> Parser<'b>) -> Parser<'b> =
+    fun pa f ->
+        fun s ->
+            let as' = pa s
+            let bs = seq { for (a',s') in as' -> (f a') s' }
+            List.concat bs
+
+/// Infix shorthand for bind
+let (>>=) = bind
+
+/// A trivial Parser that ignores the input string and produces the given symbol
+let retParser: 'a -> Parser<'a> =
+    fun a -> fun s -> [(a, s)]
+
+/// Apply for parsers
+let ap: Parser<'a -> 'b> -> Parser<'a> -> Parser<'b> =
+    fun pf pa -> pf >>= fun f -> pa >>= fun a -> retParser (f a)
+
+/// Infix shorthand for ap
+let (<*>) = ap
+
+
